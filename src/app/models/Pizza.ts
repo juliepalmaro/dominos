130cf0f2e99652b03676402f2679f9429bb4d123
C:\xampp\htdocs <julie.palmaro@gmail.com>
2020-04-17 13:48:17 +0200

    page details pizza

--- src/app/models/Pizza.ts
+++ src/app/models/Pizza.ts
@@ -5,10 +5,11 @@ export default class Pizza {
     price: number;
     ingredients: Array<string>;
 
-    constructor(id: string, picture: string, name: string, price: number) {
+    constructor(id: string, picture: string, name: string, price: number, ingredients: Array<string>) {
         this.id = id;
         this.picture = picture;
         this.name = name;
         this.price = price;
+        this.ingredients = ingredients;
     }
 }
--- src/app/pizza/pizza.page.ts
+++ src/app/pizza/pizza.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Pizza from '../models/Pizza';
 
 @Component({
   selector: 'app-pizza',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pizza.page.scss'],
 })
 export class PizzaPage implements OnInit {
+  pizza: Pizza;
 
+  // Injection de dépendances
   constructor() { }
 
+  // Initialisation des variables
   ngOnInit() {
+    this.getOne('1');
+  }
+
+  getOne(idP: string) {
+    this.pizza = {
+      id: idP,
+      picture: 'data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBw0HBwcHBw0NBwcHBw0HBwcHDQ8IDQcNFREWFhURFRUYHSggGBoxJxUVITEhMSkrLi4uFx8zODMtNygtOisBCgoKBQYFDgUFDisZExkrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrKysrK//AABEIAOEA4QMBIgACEQEDEQH/xAAWAAEBAQAAAAAAAAAAAAAAAAAAAQb/xAAUEAEAAAAAAAAAAAAAAAAAAAAA/8QAFQEBAQAAAAAAAAAAAAAAAAAAAAb/xAAUEQEAAAAAAAAAAAAAAAAAAAAA/9oADAMBAAIRAxEAPwDKAixTCgAAAAAIoCKAAigigAigIKAgoCKAIKAgoAioCgAAAAAAAAAAAAAAAAAAAAAAAAAAAIqAoAAAAAAAAAAAAAAAAAAAAAAAAAAACKgKAAAAAAAAAAAAAAAAAAAAAAAAAAAAioCgAAAAAAAAAAAAAAAIoAACKAAACKAgoAioAKAIoAAAAAAAAAAAAAAAAAAAAAAAAAioCgAAAAAAAAAAAAAAAAAAAAAAAAAAAIqAoigAAAAAAAAAAAAAAAAAAAAAAAAAAIqAoigAAAACKAIoAAAAAAAAAAAAAigAAAACAKAAIoAAAAAgCgAAACKAAAAAAAAAAAAAioCgAAAAAAAAAAAAAAAAAAAAAAAAAAAIqACgCKAAAAAAAAAAAAAAAAAAAAAAAAAIqAqKAIoAAAACKACKAgoAAAigAAAAAAAAAACKAAAIoAAAAAAAAAAAAAAAAAAAAAAAACCoCiAKIAAAqACiAKIAogCiAKIAqACgAIACoAqAAAD/2Q==',
+      name: 'Royale',
+      price: 12,
+      ingredients: ['champignon', 'jambon', 'rapé']
+    };
   }
 
 }
